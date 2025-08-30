92cbf2038d0f37cf4411977fadb9a897e19d939e
rabani2022 <kopsyahrabani@gmail.com>
2025-08-30 09:39:26 +0700

    solve

--- routes/web.php
+++ routes/web.php
@@ -66,8 +66,7 @@ Route::prefix('admin')->as('admin.')->middleware(['auth'])->group(function () {
       Route::get('/{client}/edit',   [AdminClientController::class, 'edit'])->name('edit');
       Route::put('/{client}',        [AdminClientController::class, 'update'])->name('update');
       Route::delete('/{client}',     [AdminClientController::class, 'destroy'])->name('destroy');
-      Route::post('/{client}/router/import-vouchers', [AdminClientsController::class, 'importVouchers'])->name('router.import-vouchers');
-
+    
       // Halaman alat router/hotspot
       Route::get('/{client}/tools',  [AdminClientController::class, 'tools'])->name('tools');
 
@@ -75,6 +74,7 @@ Route::prefix('admin')->as('admin.')->middleware(['auth'])->group(function () {
       Route::post('/{client}/router/test',                [AdminClientController::class, 'routerTest'])->name('router.test');
       Route::post('/{client}/router/hotspot-test-user',   [AdminClientController::class, 'routerHotspotTestUser'])->name('router.hotspot-test-user');
       Route::post('/{client}/router/hotspot-login-test',  [AdminClientController::class, 'routerHotspotLoginTest'])->name('router.hotspot-login-test');
+      Route::post('/{client}/router/import-vouchers',     [AdminClientsController::class, 'importVouchers'])->name('router.import-vouchers');
     });
     
     // Users Management
